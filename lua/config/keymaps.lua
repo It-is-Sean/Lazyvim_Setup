0583e7b482534e4c4e7ad18d0be2cd6ee8f47f9d
Sean <jiangyunchen2006@icloud.com>
2024-04-12 18:46:22 +0800

    config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,14 +4,32 @@
 local map = vim.keymap.set
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
+--LAZYGIT
 map("n", "<leader>gg", function()
   lazygit:toggle()
 end, { desc = "Lazygit" })
 map("n", "<leader>gG", function()
   LazyVim.lazygit()
 end, { desc = "Lazygit (cwd)" })
-map("n", "<F7>", "<cmd>ToggleTerm<CR>")
-map("n", "r", "<C-r>")
 
-map("t", "<F7>", "<cmd>ToggleTerm<CR>")
+--CODERUNNER
+map("n", "r", "<C-r>")
+map("n", "<F6>", "<cmd>CompilerOpen<cr>", { noremap = true, silent = true, desc = "Run Code" })
+map("n", "<leader>r", "<cmd>CompilerOpen<cr>", { noremap = true, silent = true, desc = "Run Code" })
+
+--DAP
+map({ "n", "i" }, "<F5>", function()
+  require("dap").continue()
+end, { desc = "Launch/Contunue Debug" })
+map({ "n", "i" }, "<f10>", function()
+  require("dap").step_over()
+end, { desc = "Continue" })
+map({ "n", "i" }, "<C-f10>", function()
+  require("dap").step_over()
+end, { desc = "Step Into" })
+map({ "n", "i" }, "<C-f5>", function()
+  require("dap").step_over()
+end, { desc = "Stop" })
+
+--TERMINAL
+map({ "t", "i", "n" }, "<F7>", "<cmd>ToggleTerm<CR>")
