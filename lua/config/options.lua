678fed36a6304422c80818960ba3728c6e694c71
Sean <jiangyunchen2006@icloud.com>
2024-05-03 20:52:06 +0800

    test

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,7 @@
+local opt = vim.opt
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "pyright"
+opt.relativenumber = false
