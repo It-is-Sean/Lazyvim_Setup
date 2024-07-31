1d1f444b1dbe43b914813e8e15d81e66b017ed99
Sean <jiangyunchen2006@icloud.com>
2024-07-31 10:44:33 +0800

    add colorscheme catppuccin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,6 @@
-local opt = vim.opt
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
 vim.g.lazyvim_python_lsp = "pyright"
-opt.relativenumber = false
+vim.opt.relativenumber = false
